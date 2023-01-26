d3e2f9953d26768670fb63ce868c48e21759c2c7
Afifp29 <muhammadafif2908@gmail.com>
2023-01-26 09:47:23 +0700

    On branch main_afif

--- assets/main/js/login.js
+++ assets/main/js/login.js
@@ -85,68 +85,73 @@ function lupa_password() {
     var postData = new FormData();
     postData.append("email", $("#email").val());
     var url = baseurl + "index.php/auth/send_recover_pass";
-    
-    $.ajax({
-        url: url,
-        type: "POST",
-        dataType: "text",
-        processData: false,
-        contentType: false,
-        async: false,
-        data: postData,
-    })
-    .done(function(resp) {
-        data = resp;
-    })
-    .fail(function() {
-        data = "Fail";
-    });
-
-    var ret = JSON.parse(data);
-    if (ret.status == 1) 
-    {
-        var timerInterval;
-        Swal.fire({
-        title: 'Login Berhasil',
-        html: 'I will close in <b></b> seconds.',
-        timer: 2000,
-        icon: 'success',
-        timerProgressBar: true,
-        didOpen:function () {
-            Swal.showLoading()
-            timerInterval = setInterval(function() {
-            var content = Swal.getHtmlContainer()
-            if (content) {
-                var b = content.querySelector('b')
-                if (b) {
-                b.textContent = Swal.getTimerLeft()
-                }
-            }
-            }, 100)
-        },
-        onClose: function () {
-            clearInterval(timerInterval)
-        }
-        }).then(function (result) {
-        /* Read more about handling dismissals below */
-        if (result.dismiss === Swal.DismissReason.timer) {
-            var url = baseurl + "index.php/home";
-            window.location.replace(url);
-        }
+    $('#btn-reset').css('display','none');
+    $('#loading').css('display','block');
+    // const data = [];
+    setTimeout(function() {
+        $.ajax({
+            url: url,
+            type: "POST",
+            dataType: "text",
+            processData: false,
+            contentType: false,
+            async: false,
+            data: postData,
         })
-
-
+        .done(function(resp) {
+            data = resp;
+        })
+        .fail(function() {
+            data = "Fail";
+        });
         
-    }
-    else
-    { 
-        Swal.fire({
-            title: ret.label,
-            text: ret.txt,
-            icon: 'error',
-            confirmButtonColor: '#5156be',
-          })
-    }
+
+        var ret = JSON.parse(data);
+        if (ret.status == 1) 
+        {
+            var timerInterval;
+            Swal.fire({
+            title: 'Berhasil Reset Password',
+            html: 'Link reset password telah dikirim ke '+ ret.to +', Pesan ini akan otomatis tertutup dalam <b></b> detik.',
+            timer: 5000,
+            icon: 'success',
+            timerProgressBar: true,
+            didOpen:function () {
+                Swal.showLoading()
+                timerInterval = setInterval(function() {
+                var content = Swal.getHtmlContainer()
+                if (content) {
+                    var b = content.querySelector('b')
+                    if (b) {
+                    b.textContent = Swal.getTimerLeft()
+                    }
+                }
+                }, 100)
+            },
+            onClose: function () {
+                clearInterval(timerInterval)
+            }
+            }).then(function (result) {
+            /* Read more about handling dismissals below */
+                if (result.dismiss === Swal.DismissReason.timer) {
+                    var url = baseurl + "index.php/auth";
+                    window.location.replace(url);
+                }
+            });
+        }
+        else
+        { 
+            Swal.fire({
+                title: "Gagal Reset Password",
+                text: "Gagal "+ret.to,
+                icon: 'error',
+                confirmButtonColor: '#5156be',
+                })
+        }
+        $('#btn-reset').css('display','block');
+        $('#loading').css('display','none');
+      }, 3000);
+      
     
 }
 
